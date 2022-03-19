e279ab4d97d97427ad5261768625eb16912eab8e
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-03-19 12:30:09 -0400

    Setting # of ch and type of ch on js

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,44 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//
+function generatePassword(){
+  
+  var numChar, numTypes;
+  var chars= {
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+
+  };
+  console.log(chars.upperCase.charAt[0]);
+  function increase (x,y){
+    if(x == true){
+      y++;
+    }
+    return (y);
+  }
+
+  do{
+    numChar= prompt("Ingrese la cantidad de caracteres que desea para su contraseña (8-128)");
+    if (numChar <= 7 || numChar >= 129) {
+      alert("Your password must have min 8 characters and max 128");
+    }
+  } while (numChar <= 7 || numChar >= 129);
+
+  do{
+    numTypes = 0;
+    let lowerCase= confirm("Would you like to include lowercase elements?");
+    numTypes = increase (lowerCase,numTypes);
+    let upperCase = confirm("Would you like to include uppercase elements?");
+    numTypes = increase (upperCase,numTypes);
+    let numbers = confirm("Would you like to include numbers?");
+    numTypes = increase (numbers,numTypes);
+    let specialChar = confirm("Would you like to include special characters?");
+    numTypes = increase (specialChar,numTypes);
+    if (numTypes == 0){
+      alert("Your password must include at least one type of characters");
+    }
+
+  } while (numTypes <1);
+}
---
+++ Develop/script1.js
@@ -0,0 +1,6 @@
+var num, result,i;
+num= Number(prompt("What's the number?"));
+ for (i=1; i<=num; i++){
+     result += i;
+ }
+ console.log(result);
