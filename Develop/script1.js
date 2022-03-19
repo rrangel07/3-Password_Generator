e22f176460bc084e983aef2a1e48f0519acc1acc
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-03-19 19:49:08 -0400

    Deleting unnecesary files and moving index.html to root folder

--- Develop/script1.js
+++
@@ -1,8 +0,0 @@
-
-
-  var chars= {
-    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    lowerCase: "abcdefghijklmnopqrstuvwxyz",
-    numbers: "0123456789",
-    specialChar: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
-  };
